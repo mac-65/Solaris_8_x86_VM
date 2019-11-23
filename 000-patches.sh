f97c8bd2c89cdb29495d1b58d4728418ecbefad7
bk <bk@65C02.org>
2019-11-23 14:23:36 -0500

    VirtualBox :: settings documented

--- 000-patches.sh
+++ 000-patches.sh
@@ -8,13 +8,11 @@
 # The complaint was that each directory was invalid.  The 'net offered nothing!
 # So, I had to write this to use '/usr/sbin/pkgadd' - I think it'll be okay...
 #
-# So, the patch cluster addresses the main issue of '/dev/random' which is
-# needed for openssl, et. al.  Hopefully, these patches will really work!
-#
 
 ATTR_OFF="`tput sgr0`" ;
 ATTR_BOLD="${ATTR_OFF}`tput bold`" ; # shelltool has no support for attributes :(.
 ATTR_RED_BOLD="${ATTR_OFF}`tput setaf 1; tput bold`" ;
+ATTR_RED="${ATTR_OFF}`tput setaf 1`" ;
 ATTR_GREEN="${ATTR_OFF}`tput setaf 2`" ;
 ATTR_GREEN_BOLD="${ATTR_OFF}`tput setaf 2; tput bold`" ;
 ATTR_YELLOW="${ATTR_OFF}`tput setaf 3`" ;
@@ -26,9 +24,24 @@ PKGADD_ADMIN='/root/pkgadd-admin' ;
 PKGADD='/usr/sbin/pkgadd' ;
 GREP='/usr/xpg4/bin/grep' ;
 
+###############################################################################
+# Skip running the 'prepatch' script if it's there (seems to go better).
+#
+SKIP_PREPATCH=1 ;
+
+###############################################################################
+# I have absolutely no idea where this is *supposed* to be set, but the patches
+# need it in particular for the 'postpatch' scripts (there aren't many, but
+# 112439 runs a 'postpatch' script and the '/dev/random' and '/dev/urandom'
+# will NOT be built correctly (after rebooting) if it is not run).
+#
+ROOTDIR='/' ;
+export ROOTDIR ;
+
 PKG_LOG_FILE="/root/`basename $0 .sh`-log.txt" ;
 PKG_DELAY='' ;
 PKG_TALLY=0 ;
+PKG_NONZERO=0 ;
 PKG_ERRS=0 ;
 
 PART=1;
@@ -51,7 +64,7 @@ if [ "${ANS}" = 'y' ] ; then # {
  #                                                            #
  # The first part will:                                       #
  # - mkdir -p /root/bin                                       #
- # - copy scripts and package directories to /root            #
+ # - copy required scripts and package directories to /root   #
  # - enter maintenance mode using 'init S'                    #
  #                                                            #
  # The 2nd part will add the patches using '`tput setaf 3`/usr/sbin/pkgadd`tput sgr0`' #
@@ -77,7 +90,7 @@ HERE_DOC
     fi # }
   else # }{
     printf '\n%s? ' \
-       "${ATTR_BOLD}Enter delay (seconds to sleep between packages)${ATTR_OFF}" ;
+       "${ATTR_BOLD}Enter delay (seconds to sleep between packages), RETURN for none${ATTR_OFF}" ;
     read PKG_DELAY ;
   fi # }
 
@@ -96,7 +109,9 @@ HERE_DOC
     for pkg in 8_x86_Recommended              \
                J2SE_Solaris_8_x86_Recommended \
                000-patches.sh                 \
-               pkgadd-admin                   ; do
+               pkgadd-admin                   \
+               snippets.sh                    \
+        ; do
       printf "${ATTR_CYAN_BOLD}%s${ATTR_OFF} .." "${pkg}" ;
         tar cf - "${pkg}" | ( cd /root && tar xf - ) ;
       printf "\r${ATTR_GREEN_BOLD}%s${ATTR_OFF} ...\n" "${pkg}" ;
@@ -116,27 +131,53 @@ HERE_DOC
     # For some reason, 'install_cluster' does NOT work even with the addition
     # of '-M'.  This part was trial and error, and even though it appears to
     # work, I don't know if it's correct.  But things DO seem to work "better"
-    # after the patches are installed, so I guess that's SUCCESSFUL indicator.
+    # after the patches are applied, so I guess that's a SUCCESSFUL indicator.
     #
-    for patch_dir in `cat patch_order` ; do
+    # Everything in 'patch_order' is guaranteed to be a directory...
+    #
+    for patch_dir in `cat patch_order` ; do # {
+
+       ########################################################################
+       # There aren't many pre/post scripts and should be run for completeness.
+       #
        patch_dev="/root/${recommended}/${patch_dir}" ;
-       cd "${patch_dev}" ;
-       for patch in `ls -d S*` ; do
+       cd "${patch_dev}" ; # in patch directory, check and run './prepatch'...
+       {
+          echo ;
+          echo '###########################################################' ;
+          echo '###########################################################' ;
+          printf "IN '%s'\n" "`/bin/pwd`" ;
+          if [ ${SKIP_PREPATCH} -eq 0 -a -x ./prepatch ] ; then
+
+             echo "RUNNING './prepatch'..." ;
+             ./prepatch ;
+             if [ $? -ne 0 ] ; then
+                echo "SKIPPING '${patch_dev}' -- './prepatch' returned NON-ZERO" ;
+                continue ;
+             fi ;
+          fi ;
+       } 2>&1 | tee -a "${PKG_LOG_FILE}" \
+              | sed -e "s/^RUNNING/${ATTR_YELLOW}RUNNING${ATTR_OFF}/" \
+                    -e "s#^SKIPPING#${ATTR_RED}SKIPPING${ATTR_OFF}#" ;
+
+       for patch in `ls -d S*` ; do # {
+         [ ! -d "${patch}" ] && continue ; # it's NOT a directory, then skip it
 
          PKG_TALLY=`expr ${PKG_TALLY} + 1` ;
 
          { echo ; echo ;
-         echo '###########################################################' ;
-         echo '###########################################################' ; } \
+         echo '#############################################' ; } \
            >> "${PKG_LOG_FILE}" ;
 
          { /usr/sbin/pkgadd -a /root/pkgadd-admin -d "${patch_dev}" ${patch} ;
-           RC=$? ; echo ${RC} > /root/RC$$ ; } 2>&1 | tee -a "${PKG_LOG_FILE}" ;
-         SLEEP_TIME=${PKG_DELAY} ;
-         RC=`cat /root/RC$$` ;
-         echo "RC = ${RC}" >> "${PKG_LOG_FILE}" ;
+           RC=$? ; echo ${RC} > /root/RC$$ ;
+         } 2>&1 | tee -a "${PKG_LOG_FILE}" ;
 
+         SLEEP_TIME=${PKG_DELAY} ;
+
+         RC=`cat /root/RC$$` ;
          printf "${RC}, " ;
+
          case $RC in
            0) printf "${ATTR_GREEN}%s - ${ATTR_BLUE_BOLD}%s${ATTR_OFF}\n" \
                 'SUCCESS' "${patch_dir}/${patch}" ;
@@ -145,60 +186,53 @@ HERE_DOC
            # We'll see '4' if the package was already installed (so it seems).
            [24]) printf "${ATTR_YELLOW}%s - ${ATTR_BLUE_BOLD}%s${ATTR_OFF}\n" \
                 'ALREADY INSTALLED' "${patch_dir}/${patch}" ;
-              PKG_ERRS=`expr ${PKG_ERRS} + 1` ;
+              PKG_NONZERO=`expr ${PKG_NONZERO} + 1` ;
               ;;
            6) printf "${ATTR_YELLOW}%s - ${ATTR_BLUE_BOLD}%s${ATTR_OFF}\n" \
                 'OBSOLETE' "${patch_dir}/${patch}" ;
-              PKG_ERRS=`expr ${PKG_ERRS} + 1` ;
+              PKG_NONZERO=`expr ${PKG_NONZERO} + 1` ;
               ;;
            8) printf "${ATTR_YELLOW}%s - ${ATTR_CYAN_BOLD}%s${ATTR_OFF}\n" \
                 'NOT INSTALLED' "${patch_dir}/${patch}" ;
-              PKG_ERRS=`expr ${PKG_ERRS} + 1` ;
+              PKG_NONZERO=`expr ${PKG_NONZERO} + 1` ;
               ;;
            13) printf "${ATTR_YELLOW}%s - ${ATTR_CYAN_BOLD}%s${ATTR_OFF}\n" \
                 'SYMBOLIC LINK?' "${patch_dir}/${patch}" ;
-              PKG_ERRS=`expr ${PKG_ERRS} + 1` ;
+              PKG_NONZERO=`expr ${PKG_NONZERO} + 1` ;
               ;;
            18) printf "${ATTR_RED_BOLD}%s - ${ATTR_BOLD}%s${ATTR_OFF}\n" \
                 'NO DISK SPACE' "${patch_dir}/${patch}" ;
-              PKG_ERRS=`expr ${PKG_ERRS} + 1` ;
+              PKG_NONZERO=`expr ${PKG_NONZERO} + 1` ;
               ;;
            25) printf "${ATTR_CYAN_BOLD}%s - ${ATTR_CYAN_BOLD}%s${ATTR_OFF}\n" \
                 'MISSING DEPENDENCY' "${patch_dir}/${patch}" ;
-              PKG_ERRS=`expr ${PKG_ERRS} + 1` ;
+              PKG_NONZERO=`expr ${PKG_NONZERO} + 1` ;
               ;;
-           *) printf "${ATTR_RED_BOLD}%s - ${ATTR_BOLD}%s${ATTR_OFF}\n" \
+           *) printf "FATAL -- "  >> "${PKG_LOG_FILE}" ;
+              printf "${ATTR_RED_BOLD}%s - ${ATTR_BOLD}%s${ATTR_OFF}\n" \
                 'ERROR' "${patch_dir}/${patch}" ;
-              SLEEP_TIME=5 ;
+              SLEEP_TIME=3 ;
               PKG_ERRS=`expr ${PKG_ERRS} + 1` ;
               ;;
          esac
-         if [ "${SLEEP_TIME}" != '' ] ; then sleep "${SLEEP_TIME}" ; fi
-       done
-    done
-   done # }
+         echo "RC = ${RC}" >> "${PKG_LOG_FILE}" ;
 
-   ############################################################################
-   # Build the links for '/dev/random' and '/dev/urandom' if 112439 present.
-   # We can do everything EXCEPT /usr/sbin/drvconfig (which caused a kernel
-   # panic) probably because we had to reboot first).
-   #
-   showrev -p | ${GREP} -q 112439 ; RC=$? ;
-   if [ ${RC} -eq 0 -a ! -h /dev/random ] ; then
-     printf 'FIXING :: /dev/random and /dev/urandom ...\n' ;
-     /usr/sbin/rem_drv random ;
-     /bin/rm -f /dev/*random ;
-     /usr/sbin/add_drv -m '* 644 root sys' random ; # Adds the pseudo devices
- ### /usr/sbin/drvconfig
-     ( cd /dev ; ln -s '/devices/pseudo/random@0:random' random ;
-                 ln -s '/devices/pseudo/random@0:urandom' urandom )
-     ls -ld /dev/*random | cut -c54- \
-        | sed -e "s/^/${ATTR_GREEN}SUCCESS${ATTR_OFF} ==>/" ;
-     echo ;
-   else
-     printf "${ATTR_RED_BOLD}%s${ATTR_OFF}\n\n" \
-            'Installation of 112439 has FAILED!' ;
-   fi
+         if [ "${SLEEP_TIME}" != '' ] ; then sleep "${SLEEP_TIME}" ; fi
+       done # }
+
+       ########################################################################
+       # Run the 'postpatch' script if it's there.
+       # This _should_ properly setup the /dev/random links I was missing...
+       #
+       {  if [ -x postpatch ] ; then
+
+          echo "RUNNING './postpatch'..." ;
+          ./postpatch ;
+       fi ; } 2>&1 | tee -a "${PKG_LOG_FILE}" \
+                   | sed -e "s/^RUNNING/${ATTR_BLUE_BOLD}RUNNING${ATTR_OFF}/" ;
+
+    done # }
+   done # }
 
    ############################################################################
    #
@@ -206,14 +240,15 @@ HERE_DOC
 `tput smso; tput bold`/************************************\\
 **************************************
 **  ALL PATCHES HAVE BEEN APPLIED.  **
-**  `printf '%3d' ${PKG_TALLY}` packages were processed,    **
-**  `printf '%3d' ${PKG_ERRS}` had a non-zero status.      **
+**  `printf '%3d' ${PKG_TALLY}` packages were processed:    **
+**  `printf '%3d' ${PKG_NONZERO}` had a non-zero status,      **
+**  `printf '%3d' ${PKG_ERRS}` had a fatal error status.   **
 **************************************
 \************************************/`tput sgr0`
 
 (See '${PKG_LOG_FILE}' for details.)
 
-You should change root's home directory to '/root' in '/etc/passwd', REBOOT.
+You should change root's home directory to '/root' in '/etc/passwd', ${ATTR_BOLD}REBOOT!${ATTR_OFF}.
 
 HERE_DOC
 
